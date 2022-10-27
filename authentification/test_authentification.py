f1211638b59aea669057fdd56b32caba70f33e50
Hag-Zilla <d.mascheix@gmail.com>
2022-10-27 19:11:56 +0000

    add docstrings

--- authentification/test_authentification.py
+++ authentification/test_authentification.py
@@ -13,7 +13,6 @@ Notes :
 """
 # ================================================    Modules import     =====================================================
 
-
 import os
 import requests
 import json
@@ -22,7 +21,31 @@ today = datetime.now()
 
 # ================================================          Functions / Class          ================================================
 
-def auth_request (username,password,exp_status_code,output):
+# définition de l'adresse de l'API
+api_address = '172.17.0.2'
+# port de l'API
+api_port = 8000
+# node definition
+api_node = "/permissions"
+
+test_name = "Authentication test"
+
+def auth_request (api_address,api_port,api_node,test_name,username,password,exp_status_code,output,date):
+    """_summary_
+    
+    auth_request : Function that manage the authorization request. It will give a full log if the "LOG" environment variable is set to 1.
+
+    Args:
+        api_address (string): The API adress to request
+        api_port (integer): The API port to request
+        api_node (string): The endpoint to request
+        test_name (string): The test name
+        username (string): username to test
+        password (string): password to test
+        exp_status_code (integer): expected status code
+        output (string): formated string with the following parameters : {date} {node} {user} {pwd} {exp_status_code} {status_code} {test_status}
+        date (string): date of the day
+    """
     try:
         r = requests.get(url='http://{address}:{port}{node}'.format(address=api_address, port=api_port, node = api_node),
                     params= {'username': username,
@@ -44,7 +67,7 @@ def auth_request (username,password,exp_status_code,output):
 
     # Formating output
     output = output.format(test_name = test_name,
-                           date = today,
+                           date = date,
                            node = api_node,
                            user = username,
                            pwd= password,
@@ -105,10 +128,15 @@ with open('test_credentials.json') as json_file:
 
 # Loop to go through all the test credentials        
 for name in test_credentials:
-    auth_request (username = name,
+    auth_request (api_address = api_address,
+                  api_port = api_port,
+                  api_node = api_node,
+                  test_name = test_name,
+                  username = name,
                   password = test_credentials[name]["pwd"],
                   exp_status_code = test_credentials[name]["expected_code"],
-                  output=output
+                  output = output,
+                  date = today
                   )
      
 # ===========================================================================================================================
